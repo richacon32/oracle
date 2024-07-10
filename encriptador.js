b4406e2bd7ae0ec58258675bdd0c2b39526b49ed
richacon32 <richychacon32@gmail.com>
2024-07-09 19:24:37 -0500

    Primer Proyecto

---
+++ encriptador.js
@@ -0,0 +1,109 @@
+function encriptar(traduccion){
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#texto");
+    const texto = textarea.value;
+    var area_default = document.querySelector("#default");
+    var area_result = document.querySelector("#result");
+    var texto_out = document.querySelector("#texto_out");
+    var exclamacion = document.querySelector("#exclamacion");
+    if (texto != ""){
+        var out = ""
+        for(var i=0; i < texto.length; i++){
+            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
+                document.querySelector("#warning").style.color = "white";
+                document.querySelector("#warning").style.fontSize = "16px";
+                exclamacion.style.display = "inline";  // Muestra la imagen
+                return;
+            }
+            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+            if(texto[i] == 'a'){
+                out += traduccion["a"] ;
+            }
+            else if(texto[i] == 'e'){
+                out += traduccion["e"];
+            }
+            else if(texto[i] == 'i'){
+                out += traduccion["i"]; 
+            }
+            else if(texto[i] == 'o'){
+                out += traduccion["o"]; 
+            }
+            else if(texto[i] == 'u'){
+                out += traduccion["u"]; 
+            }
+            else{
+                out += texto[i];
+            }
+        }
+
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto_out.innerHTML = out;
+        
+        // Cambia la imagen y ajusta el ancho
+        var headerLogo = document.getElementById("headerLogo");
+        headerLogo.src = "./img/aluraoracle.jpg";
+        headerLogo.style.width = "200px";
+    }
+
+    return;
+}
+
+function desencriptar(traduccion){
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#texto");
+    var texto = textarea.value;
+    var area_default = document.querySelector("#default");
+    var area_result = document.querySelector("#result");
+    var texto_out = document.querySelector("#texto_out");
+    var exclamacion = document.querySelector("#exclamacion");
+    if (texto != ""){
+        for(var i=0; i < texto.length; i++){
+            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
+                document.querySelector("#warning").style.color = "white";
+                document.querySelector("#warning").style.fontSize = "16px";
+                exclamacion.style.display = "inline";  // Muestra la imagen
+                return;
+            }
+            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+        }
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto = texto.replace(new RegExp(traduccion["a"], "g"), "a");
+        texto = texto.replace(new RegExp(traduccion["e"], "g"), "e");
+        texto = texto.replace(new RegExp(traduccion["i"], "g"), "i");
+        texto = texto.replace(new RegExp(traduccion["o"], "g"), "o");
+        texto = texto.replace(new RegExp(traduccion["u"], "g"), "u");
+        texto_out.innerHTML = texto;
+
+        // Cambia la imagen y ajusta el ancho
+        var headerLogo = document.getElementById("headerLogo");
+        headerLogo.src = "./img/aluraoracle.jpg";
+        headerLogo.style.width = "200px";
+    }
+    return;
+}
+
+
+function clipboard(){
+    const texto_out = document.querySelector("#texto_out");
+    navigator.clipboard.writeText(texto_out.value);
+}
+
+const enc = document.querySelector('#enc');
+const des = document.querySelector('#des');
+const copy = document.querySelector('#copiar');
+
+var traduccion = {"a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat"};
+
+enc.addEventListener( 'click', function() {encriptar(traduccion);} );
+des.addEventListener( 'click', function() {desencriptar(traduccion);} );
+copy.addEventListener( 'click', function() {clipboard();} );
